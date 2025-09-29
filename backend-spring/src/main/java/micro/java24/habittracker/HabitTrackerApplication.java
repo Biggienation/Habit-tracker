314505e70b7bd5dca3c56719632065e48cb36b60
Alexander Pettersson <alexpet5888@gmail.com>
2025-09-29 17:26:31 +0200

    Add files via upload

---
+++ backend-spring/src/main/java/micro/java24/habittracker/HabitTrackerApplication.java
@@ -0,0 +1,13 @@
+package micro.java24.habittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitTrackerApplication.class, args);
+    }
+
+}
---
+++ backend-spring/src/test/java/micro/java24/habittracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.java24.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
